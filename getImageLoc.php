953551a4d718a4c0d271bcacbf28bbe8fb0fe998
moonsoo101 <moonsoo101@gmail.com>
2017-06-07 23:06:37 +0000

    블랙박스 마무리

---
+++ getImageLoc.php
@@ -0,0 +1,17 @@
+<?php
+	$imgName = $_POST["post1"];
+		$con = mysqli_connect("localhost", "root", "leastb", "fcm");
+ 		mysqli_set_charset($con,"utf8");
+ 		if (mysqli_connect_errno($con))
+ 		{
+ 		   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ 		}
+ 		$query = "select latitude, longitude from image where imgname ='$imgName';";
+ 		$res = mysqli_query($con,$query);
+ 		$result = array();
+ 		while($row = mysqli_fetch_array($res)){
+ 		  array_push($result, array('latitude'=>$row[0],'longitude'=>$row[1]));
+ 		}
+ 		echo json_encode(array("result"=>$result));
+ 		mysqli_close($con);
+?>
--- registImage.php
+++ registImage.php
@@ -4,10 +4,16 @@ $id = $_POST['post2'];
 $year = $_POST['post3'];
 $month = $_POST['post4'];
 $day = $_POST['post5'];
+$latitude = $_POST['post6'];
+$longitude = $_POST['post7'];
+// $index = 1;
 // $id ='leastb';
 // $year = 2017;
 // $month = 6;
 // $day = 6;
+// $latitude = '34.2';
+// $longitude = '34.2';
+
 $imgName = $id.$year.$month.$day.'-'.$index;
 	$con = mysqli_connect("localhost", "root", "leastb", "fcm");
 	mysqli_set_charset($con,"utf8");
@@ -20,7 +26,7 @@ $imgName = $id.$year.$month.$day.'-'.$index;
 	$row = mysqli_fetch_array($res);
 	$WDindex = $row['WDindex'];
 	echo $WDindex."  ".$imgName;
-	$query = "insert into image (watchdayID, imgname) values ($WDindex, '$imgName');";
+	$query = "insert into image (watchdayID, imgname, latitude, longitude) values ($WDindex, '$imgName', '$latitude', '$longitude');";
 	$res = mysqli_query($con,$query);
 	mysqli_close($con);
 ?>
